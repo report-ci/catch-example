f4e6430c6bb68725993fc52d80cdef1bc7a907ee
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-29 19:30:59 +0800

    set static test to runtime && removed double test run

--- test/catch2.cpp
+++ test/catch2.cpp
@@ -1,4 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNTIME_STATIC_REQUIRE
 #include <catch2/catch.hpp>
 #include <type_traits>
 
