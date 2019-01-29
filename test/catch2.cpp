06defeae6cdc09ff4706f19a692af39b83017f92
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-29 19:03:27 +0800

    Broke a few tests

--- test/catch2.cpp
+++ test/catch2.cpp
@@ -1,9 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNTIME_STATIC_REQUIRE
 #include <catch2/catch.hpp>
 #include <type_traits>
 
 TEST_CASE("STATIC_REQUIRE showcase", "[traits]")
 {
-    STATIC_REQUIRE( std::is_void<void>::value );
-    STATIC_REQUIRE_FALSE( std::is_void<int>::value );
+    STATIC_REQUIRE( std::is_void<int>::value );
+    STATIC_REQUIRE_FALSE( std::is_void<void>::value );
 }
